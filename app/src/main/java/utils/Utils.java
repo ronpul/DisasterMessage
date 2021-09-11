a82cb2452766e6998c0d727ac8da2504600a3310
hihi0677 <hihi0677@gmail.com>
2021-09-11 20:49:23 +0900

    불필요한 소스 제거

--- app/src/main/java/utils/Utils.java
+++ app/src/main/java/utils/Utils.java
@@ -6,35 +6,15 @@ import android.net.NetworkInfo;
 
 import com.disaster.message.DMApplication;
 
-import java.util.Random;
-
 public class Utils {
-
-    private static final String ALLOWED_CHARACTERS = "0123456789qwertyuiopasdfghjklzxcvbnm";
-
-    public static String getRandomString(final int sizeOfRandomString) {
-        final Random random = new Random();
-        final StringBuilder sb = new StringBuilder(sizeOfRandomString);
-        for (int i = 0; i < sizeOfRandomString; ++i)
-            sb.append(ALLOWED_CHARACTERS.charAt(random.nextInt(ALLOWED_CHARACTERS.length())));
-        return sb.toString();
-    }
-
     public static Boolean isNetworkConnected() {
         try {
             ConnectivityManager cm = (ConnectivityManager) DMApplication.getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
             return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
         } catch (Exception e) {
+            CustomLogger.logException(e);
         }
         return false;
-    }
-
-    public static String getDisplayText(Double count) {
-        if (count > 1000) {
-            return String.format("%.1f", count / 1000) + "K";
-        } else {
-            return count.intValue() + "";
-        }
     }
 }
